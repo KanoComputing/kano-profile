b44c1066a35ef425a2b15194cda2e35e85c3b31b
Zsolt Ero <zsolt.ero@gmail.com>
2014-03-13 18:46:07 +0000

    button selector works

--- kano_profile_gui/badges.py
+++ kano_profile_gui/badges.py
@@ -1,2 +1,22 @@
-def activate():
+#!/usr/bin/env python
+
+# badges.py
+#
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+
+from gi.repository import Gtk
+
+
+def activate(_win, _table, _box):
     print __name__
+    # Table
+    table = Gtk.Table(4, 1, True)
+    _box.add(table)
+
+    # Label
+    label = Gtk.Label()
+    label.set_text("Badges")
+    table.attach(label, 0, 1, 0, 1)
+

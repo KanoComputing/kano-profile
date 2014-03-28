928036fa9a7fceecb303c86ef4d94badac9e61fa
Zsolt Ero <zsolt.ero@gmail.com>
2014-03-28 15:20:17 +0000

    missing icon added

--- kano_profile_gui/images.py
+++ kano_profile_gui/images.py
@@ -14,14 +14,17 @@ def get_image(name, pre):
     filename = '{}_{}.png'.format(pre, name)
     fullpath = os.path.join(icon_dir, filename)
     if not os.path.exists(fullpath):
-        # TODO: remove random avatar generation from production!
-        from randomavatar.randomavatar import Avatar
-        avatar = Avatar(rows=10, columns=10)
-        image_byte_array = avatar.get_image(
-            string=filename,
-            width=108, height=108, pad=10)
-        avatar.save(
-            image_byte_array=image_byte_array,
-            save_location=fullpath)
-        print '{} created'.format(fullpath)
+        try:
+            # TODO: remove random avatar generation from production!
+            from randomavatar.randomavatar import Avatar
+            avatar = Avatar(rows=10, columns=10)
+            image_byte_array = avatar.get_image(
+                string=filename,
+                width=108, height=108, pad=10)
+            avatar.save(
+                image_byte_array=image_byte_array,
+                save_location=fullpath)
+            print '{} created'.format(fullpath)
+        except Exception:
+            return '/usr/share/kano-profile/media/icons/_missing.png'
     return fullpath

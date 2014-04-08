fd7c357948756ca29c2b76b8c860d5fb0c05e91c
carolineclark <caroline@kano.me>
2014-04-08 10:23:24 +0100

    Basic styling of buttons along top bar, with properties that when active they go white, otherwise are grey

---
+++ kano_profile_gui/components/home_button.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# home_button.py
+#
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+# This controls the button styling in the default introduction screen which shows all the settings
+
+from gi.repository import Gtk
+import kano_profile_gui.components.constants as constants
+
+
+class Home_button():
+    def __init__(self, level_number):
+
+        self.possible_titles = ["White belt", "Quick learner", "Dragonslayer"]
+
+        # Contains the info about the level and the image
+        self.container = Gtk.Grid()
+
+        # Info about the different settings
+        self.title = Gtk.Label(self.possible_titles[level_number - 1])
+        self.title.get_style_context().add_class("earned_title")
+        self.title.set_alignment(xalign=0, yalign=1)
+
+        self.description = Gtk.Label("Level " + str(level_number))
+        self.description.get_style_context().add_class("level_label")
+        self.description.set_alignment(xalign=0, yalign=0)
+
+        self.button = Gtk.Button()
+        self.button.get_style_context().add_class("top_bar_button")
+        self.button.set_can_focus(False)
+        self.img = Gtk.Image()
+        self.img.set_from_file(constants.media + "/icons/Level-" + str(level_number) + ".png")
+
+        self.container.attach(self.title, 2, 0, 1, 1)
+        self.container.attach(self.description, 2, 1, 1, 1)
+        self.container.attach(self.img, 0, 0, 2, 2)
+        #self.container.set_row_spacing(5)
+        self.container.set_column_spacing(10)
+        self.container.props.valign = Gtk.Align.CENTER
+
+        self.button.add(self.container)
+
+        self.button.height = 100
+        self.button.width = 200
+
+        self.button.set_size_request(self.button.width, self.button.height)
+
+    def update_level(self, level_number):
+        self.level_image.set_from_file(constants.media + "/icons/Level-" + str(level_number) + ".png")
+        self.level.set_text("Level " + str(level_number))
+        self.earned_title.set_text(self.possible_titles[level_number])
---
+++ kano_profile_gui/components/top_bar.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+# top_bar.py
+#
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+# This controls the styling of the (pretend) top window bar.
+
+from gi.repository import Gtk, Gdk
+import kano_profile_gui.components.icons as icons
+import kano_profile_gui.components.home_button as home_button
+
+TOP_BAR_HEIGHT = 100
+SPACE_TAKEN = 220
+HEADER_SPACE = 25
+
+
+class Top_bar():
+    def __init__(self, WINDOW_WIDTH):
+
+        # Makes it easier to centre other widgets even if we change this
+        self.height = TOP_BAR_HEIGHT
+
+        # This is to give the correct colour of the top bar as Event Boxes are the only containers that we can colour
+        # This contains everything, but can't pack directly into as is only a simple container
+        self.background = Gtk.EventBox()
+        self.background.set_size_request(WINDOW_WIDTH, TOP_BAR_HEIGHT)
+        self.background.style = self.background.get_style_context()
+        self.background.style.add_class('top_bar_container')
+
+        self.container = Gtk.Grid()
+
+        # Home button
+        self.home_button = home_button.Home_button(1)
+
+        name_array = ['Badges', 'Swag', 'Challenges']
+        self.badges_button = Gtk.Button()
+        self.swag_button = Gtk.Button()
+        self.challenges_button = Gtk.Button()
+        self.button_array = [self.badges_button, self.swag_button, self.challenges_button]
+        badges_label = Gtk.Label()
+        swag_label = Gtk.Label()
+        challenges_label = Gtk.Label()
+        self.label_array = [badges_label, swag_label, challenges_label]
+
+        for x in range(3):
+            # Badges Button
+            label = self.label_array[x]
+            label.set_text(name_array[x].upper())
+            label.get_style_context().add_class("top_bar_label")
+
+            icon = Gtk.Image()
+            icon.set_from_pixbuf(icons.Icons(name_array[x].lower()).subpixbuf)
+
+            container = Gtk.Box()
+            container.pack_start(icon, False, False, 0)
+            container.pack_start(label, False, False, 0)
+
+            button = self.button_array[x]
+            button.set_size_request(TOP_BAR_HEIGHT, TOP_BAR_HEIGHT)
+            button.set_can_focus(False)
+            button.get_style_context().add_class("top_bar_button")
+            button.add(container)
+            button.connect('button_press_event', self.activate_label)
+
+        # Close button
+        cross = Gtk.Image()
+        cross.set_from_pixbuf(icons.Icons("cross").subpixbuf)
+
+        close_button = Gtk.Button()
+        close_button.set_image(cross)
+        close_button.set_size_request(15, 15)
+        close_button.set_can_focus(False)
+        close_button.get_style_context().add_class("top_bar_button")
+        close_button.connect("button_press_event", close_window)
+
+        # Define progress bar here
+
+        self.container.attach(self.home_button.button, 0, 0, 1, 3)
+        self.container.attach(self.badges_button, 1, 0, 1, 3)
+        self.container.attach(self.swag_button, 2, 0, 1, 3)
+        self.container.attach(self.challenges_button, 3, 0, 1, 3)
+        self.container.attach(close_button, 4, 0, 1, 1)
+        self.container.set_row_spacing(5)
+        self.container.set_size_request(WINDOW_WIDTH, TOP_BAR_HEIGHT)
+
+        self.background.add(self.container)
+
+    def activate_label(self, widget, event):
+
+        for label in self.label_array:
+            label.get_style_context().remove_class("top_bar_label_active")
+            label.get_style_context().add_class("top_bar_label")
+            # Doesn't matter the colour you pass here, just needs to force Gtk to re-style the label and notice the change of class
+            label.modify_fg(Gtk.StateFlags.NORMAL, Gdk.color_parse("green"))
+
+        index = self.button_array.index(widget)
+
+        label_style = self.label_array[index].get_style_context()
+
+        label_style.remove_class("top_bar_label")
+        label_style.add_class("top_bar_label_active")
+
+
+# On closing window, will alert if any of the listed booleans are True
+def close_window(event, button):
+        Gtk.main_quit()
+
+

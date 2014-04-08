19032e717d3674dbb6165300eec7f90d0ed13492
Zsolt Ero <zsolt.ero@gmail.com>
2014-04-08 22:21:07 +0100

    import changed

--- kano/profile/__init__.py
+++ kano/profile/__init__.py
@@ -10,9 +10,10 @@ from __future__ import division
 
 import sys
 import os
-import grp
 import json
-import kano.utils as ku
+
+from kano.utils import read_file_contents, get_device_id, get_mac_address, \
+    get_date_now, ensure_dir, get_user, get_home_by_username, is_gui
 
 
 def load_profile():
@@ -26,35 +27,35 @@ def load_profile():
     if 'email' not in data and get_email_from_disk():
         data['email'] = get_email_from_disk()
 
-    if 'device_id' not in data and ku.get_device_id():
-        data['device_id'] = ku.get_device_id()
+    if 'device_id' not in data and get_device_id():
+        data['device_id'] = get_device_id()
 
-    if 'mac_addr' not in data and ku.get_mac_address():
-        data['mac_addr'] = ku.get_mac_address()
+    if 'mac_addr' not in data and get_mac_address():
+        data['mac_addr'] = get_mac_address()
 
     return data
 
 
 def save_profile(data):
-    data['last_save_date'] = ku.get_date_now()
+    data['last_save_date'] = get_date_now()
     write_json(profile_file, data)
 
 
 def get_email_from_disk():
     kano_email_file = os.path.join(home_directory, '.useremail')
-    return ku.read_file_contents(kano_email_file)
+    return read_file_contents(kano_email_file)
 
 
 def get_app_dir(app_name):
     app_dir = os.path.join(apps_dir, app_name)
-    ku.ensure_dir(app_dir)
+    ensure_dir(app_dir)
     return app_dir
 
 
 def get_app_data_dir(app_name):
     data_str = 'data'
     app_data_dir = os.path.join(get_app_dir(app_name), data_str)
-    ku.ensure_dir(app_data_dir)
+    ensure_dir(app_data_dir)
     return app_data_dir
 
 
@@ -86,7 +87,7 @@ def save_app_state(app_name, data):
         data['level'] = old_app_state['level']
 
     app_state_file = get_app_state_file(app_name)
-    data['last_save_date'] = ku.get_date_now()
+    data['last_save_date'] = get_date_now()
     write_json(app_state_file, data)
 
 
@@ -100,7 +101,7 @@ def save_app_state_variable(app_name, variable, value):
 
 def read_json(filepath, print_warning=False):
     try:
-        return json.loads(ku.read_file_contents(filepath))
+        return json.loads(read_file_contents(filepath))
     except Exception:
         if print_warning:
             print "Problem with opening: {}".format(filepath)
@@ -202,22 +203,11 @@ def is_unlocked():
 if __name__ == "__main__":
     sys.exit("Should be imported as module")
 
-# checking kanousers group
-try:
-    kanogrp = grp.getgrnam('kanousers')
-    kanomembers = kanogrp.gr_mem
-except KeyError:
-    sys.exit("kanousers group doesn't exist\nrun 'groupadd kanousers' first")
-
 # getting linux variables
-linux_user = ku.get_user()
-home_directory = ku.get_home_by_username(linux_user)
+linux_user = get_user()
+home_directory = get_home_by_username(linux_user)
 module_file = os.path.realpath(__file__)
 module_dir = os.path.dirname(module_file)
-
-# check if run under kanouser (or sudo-ed kanouser)
-if linux_user not in kanomembers:
-    sys.exit("You are not member of kanousers group\nrun `usermod -a -G kanousers {}` first".format(linux_user))
 
 # constructing paths of directories, files
 kanoprofile_dir_str = '.kanoprofile'
@@ -235,11 +225,11 @@ profile_file = os.path.join(profile_dir, profile_file_str)
 xp_file = '/usr/share/kano-profile/rules/xp.json'
 levels_file = '/usr/share/kano-profile/rules/levels.json'
 
-is_gui = ku.is_gui()
+is_gui = is_gui()
 
 if not os.path.exists(profile_file):
     profile = load_profile()
-    ku.ensure_dir(profile_dir)
+    ensure_dir(profile_dir)
     save_profile(profile)
 
 

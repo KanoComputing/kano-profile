450a2aecb0a5ce00bcb6bd47932095f68035734d
carolineclark <caroline@kano.me>
2014-04-09 16:38:31 +0100

    Renamed progress bar variables

--- kano_profile_gui/components/progress_bar.py
+++ kano_profile_gui/components/progress_bar.py
@@ -12,34 +12,36 @@ from gi.repository import Gtk
 
 class Bar():
     def __init__(self, input, WINDOW_WIDTH):
-        self.BAR_HEIGHT = 10
-        self.CONTAINER_HEIGHT = 30
-        self.LABEL_WIDTH = 60
+        # Height of the thin part of the progress bar
+        self.height = 10
+        # Height of the label
+        self.label_height = 30
+        self.label_width = 60
 
         # Calculate various widths of bars
-        progress_width = (WINDOW_WIDTH - self.LABEL_WIDTH) * input
-        rest_of_bar_width = (WINDOW_WIDTH - self.LABEL_WIDTH) * (1 - input)
+        progress_width = (WINDOW_WIDTH - self.label_width) * input
+        rest_of_bar_width = (WINDOW_WIDTH - self.label_width) * (1 - input)
 
         self.progress = Gtk.EventBox()
-        self.progress.set_size_request(progress_width, self.BAR_HEIGHT)
+        self.progress.set_size_request(progress_width, self.height)
         self.progress.get_style_context().add_class("progress_bar")
 
         self.rest_of_bar = Gtk.EventBox()
-        self.rest_of_bar.set_size_request(rest_of_bar_width, self.BAR_HEIGHT)
+        self.rest_of_bar.set_size_request(rest_of_bar_width, self.height)
         self.rest_of_bar.get_style_context().add_class("rest_of_bar")
 
         self.label = Gtk.Label(str(int(input * 100)) + "%")
-        self.label.set_size_request(self.LABEL_WIDTH, self.CONTAINER_HEIGHT)
+        self.label.set_size_request(self.label_width, self.label_height)
         self.label.set_alignment(xalign=0.5, yalign=0.5)
         self.label.get_style_context().add_class("white")
 
         self.label_background = Gtk.EventBox()
         self.label_background.get_style_context().add_class("progress_label")
-        self.label_background.set_size_request(self.LABEL_WIDTH, self.CONTAINER_HEIGHT)
+        self.label_background.set_size_request(self.label_width, self.label_height)
         self.label_background.add(self.label)
 
         self.fixed = Gtk.Fixed()
-        self.fixed.set_size_request(WINDOW_WIDTH, self.CONTAINER_HEIGHT)
+        self.fixed.set_size_request(WINDOW_WIDTH, self.label_height)
 
         # This background is so we can pretend to overlap with the top bar.
         #self.background = Gtk.EventBox()
@@ -48,5 +50,5 @@ class Bar():
 
         #self.fixed.put(self.background, 0, 0)
         self.fixed.put(self.label_background, progress_width, 0)
-        self.fixed.put(self.progress, 0, self.BAR_HEIGHT + 0)
-        self.fixed.put(self.rest_of_bar, progress_width + self.LABEL_WIDTH, self.BAR_HEIGHT + 0)
+        self.fixed.put(self.progress, 0, self.height + 0)
+        self.fixed.put(self.rest_of_bar, progress_width + self.label_width, self.height + 0)

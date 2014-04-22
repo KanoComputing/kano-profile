dbca758b83c46ef4d9dbbee066c340bae147554c
Zsolt Ero <zsolt.ero@gmail.com>
2014-04-22 18:43:30 +0100

    upload_workspace

--- test-upload.py
+++ test-upload.py
@@ -10,5 +10,6 @@ if not success:
     sys.exit('Login not possible, error: ' + value)
 
 # kw.glob_session.backup_content()
-print kw.glob_session.restore_content()
+# print kw.glob_session.restore_content()
+print kw.glob_session.upload_workspace('README.xml', 'title', 'appname')
 

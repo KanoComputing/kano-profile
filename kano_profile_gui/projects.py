6cbc8990a2d8a1cd279e75c20fb755ee7d68c7be
carolineclark <caroline@kano.me>
2014-04-24 15:34:37 +0100

    Added UI to Challenges/Projects screen

--- kano_profile_gui/projects.py
+++ kano_profile_gui/projects.py
@@ -9,8 +9,10 @@
 import os
 from gi.repository import Gtk
 
-from kano.profile.apps import get_app_list, get_app_data_dir
+#from kano.profile.apps import get_app_list, get_app_data_dir
 from kano.utils import run_print_output_error
+import kano_profile_gui.projects_list_components.project_list as project_list
+import kano_profile_gui.components.header as header
 
 app_profiles = {
     'make-pong': {
@@ -27,54 +29,63 @@ app_profiles = {
 }
 
 
-def activate(_win, _box, _label):
-    _label.set_text('Projects')
+def activate(_win, _box):
 
-    apps = get_app_list(include_empty=False)
+    """apps = get_app_list()
 
-    projects_list = []
-    for app in apps:
-        if app_profiles[app]['dir'] == 'kanoprofile':
-            data_dir = get_app_data_dir(app)
-        else:
-            data_dir = os.path.expanduser(app_profiles[app]['dir'])
+                projects_list = []
+                for app in apps:
+                    if app_profiles[app]['dir'] == 'kanoprofile':
+                        data_dir = get_app_data_dir(app)
+                    else:
+                        data_dir = os.path.expanduser(app_profiles[app]['dir'])
 
-        if not os.path.exists(data_dir):
-            continue
+                    if not os.path.exists(data_dir):
+                        continue
 
-        files = os.listdir(data_dir)
-        files_filtered = [f for f in files if os.path.splitext(f)[1][1:] == app_profiles[app]['ext']]
+                    files = os.listdir(data_dir)
+                    files_filtered = [f for f in files if os.path.splitext(f)[1][1:] == app_profiles[app]['ext']]
 
-        for filename in files_filtered:
-            project = dict()
-            project['app'] = app
-            project['data_dir'] = data_dir
-            project['file'] = filename
-            project['display_name'] = os.path.splitext(filename)[0]
-            projects_list.append(project)
+                    for filename in files_filtered:
+                        project = dict()
+                        project['app'] = app
+                        project['data_dir'] = data_dir
+                        project['file'] = filename
+                        project['display_name'] = os.path.splitext(filename)[0]
+                        projects_list.append(project)
 
-    if not projects_list:
-        return
+                if not projects_list:
+                    return
 
-    table = Gtk.Table(4, len(projects_list), True)
-    _box.add(table)
+                table = Gtk.Table(4, len(projects_list), True)
+                _box.add(table)
 
-    for i, project in enumerate(projects_list):
-        label = Gtk.Label()
-        label.set_text(project['app'])
-        table.attach(label, 0, 1, i, i + 1)
+                for i, project in enumerate(projects_list):
+                    label = Gtk.Label()
+                    label.set_text(project['app'])
+                    table.attach(label, 0, 1, i, i + 1)
 
-        label = Gtk.Label()
-        label.set_text(project['display_name'])
-        table.attach(label, 1, 2, i, i + 1)
+                    label = Gtk.Label()
+                    label.set_text(project['display_name'])
+                    table.attach(label, 1, 2, i, i + 1)
 
-        btn = Gtk.Button(label='Load', halign=Gtk.Align.CENTER)
-        btn.connect('clicked', load, project['app'], project['file'], project['data_dir'])
-        table.attach(btn, 2, 3, i, i + 1)
+                    btn = Gtk.Button(label='Load', halign=Gtk.Align.CENTER)
+                    btn.connect('clicked', load, project['app'], project['file'], project['data_dir'])
+                    table.attach(btn, 2, 3, i, i + 1)
 
-        btn = Gtk.Button(label='Share', halign=Gtk.Align.CENTER)
-        btn.connect('clicked', share, project['app'], project['file'])
-        table.attach(btn, 3, 4, i, i + 1)
+                    btn = Gtk.Button(label='Share', halign=Gtk.Align.CENTER)
+                    btn.connect('clicked', share, project['app'], project['file'])
+                    table.attach(btn, 3, 4, i, i + 1)"""
+
+    ################################################################################################################
+    heading = header.Header("Challenges")
+    plist = project_list.List()
+    scrolledwindow = Gtk.ScrolledWindow()
+    scrolledwindow.add_with_viewport(plist.background)
+    scrolledwindow.set_size_request(734, 590)
+    _box.pack_start(heading.box, False, False, 0)
+    _box.pack_start(scrolledwindow, False, False, 0)
+    _win.show_all()
 
 
 def load(_button, app, filename, data_dir):

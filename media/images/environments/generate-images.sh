43c674d5a9ad2ff0736de4a7fff103f85ad33cf9
carolineclark <caroline@kano.me>
2014-05-01 18:52:46 +0100

    Changed original images

--- media/images/environments/generate-images.sh
+++ media/images/environments/generate-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-kiwimasher -t png8 -s png -o -c1 -p resize_crop 230 180 --no-suffix
-kiwimasher -t png8 -s png -o -c1 -p resize_crop 460 448 --no-suffix
-kiwimasher -t png8 -s png -o -c1 -p resize_crop 734 404 --no-suffix
+kiwimasher -t png8 -s png -o -c0 -p resize_crop 230 180 --no-suffix
+kiwimasher -t png8 -s png -o -c0 -p resize_crop 460 448 --no-suffix
+kiwimasher -t png8 -s png -o -c0 -p resize_crop 734 404 --no-suffix
 
 
 

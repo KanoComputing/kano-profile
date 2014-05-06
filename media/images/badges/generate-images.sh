b5bf635debe52a4fac6305333414363f9862bbb3
Zsolt Ero <zsolt.ero@gmail.com>
2014-05-06 19:30:59 +0100

    images regenerated, locked versions added

--- media/images/badges/generate-images.sh
+++ media/images/badges/generate-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-kiwimasher -t png8 -s png -o -c1 -p resize_fill 230 180 --no-suffix
-kiwimasher -t png8 -s png -o -c1 -p resize_fill 460 448 --no-suffix
+kiwimasher -t png8 -s png -o -c$1 -p resize_fill 230 180 --no-suffix
+kiwimasher -t png8 -s png -o -c$1 -p resize_fill 460 448 --no-suffix
 
 
 

cbfba21502b8969ede173cf3a3455501b71de7a8
carolineclark <caroline@kano.me>
2014-05-14 12:28:44 +0100

    Changed which stats are displayed on the home screen and the styling

--- kano_profile_gui/home_components/home_stats.py
+++ kano_profile_gui/home_components/home_stats.py
@@ -11,32 +11,40 @@ from gi.repository import Gtk
 
 
 class HomeStats():
-    def __init__(self, WINDOW_WIDTH, stat_dict):
-
-        #stat_titles = ["Name", "Level", "Progress to next level", "Completion"]
+    def __init__(self, WINDOW_WIDTH):
 
         # Contains the info about the level and the image
         self.height = 88
         self.container = Gtk.Grid()
         self.container.set_size_request(WINDOW_WIDTH, self.height)
+
+        # Placeholder info
+        blocks_used = 2355
+        completed_challenges = 19
+        shares = 10
+
+        # Stats
+        stat_dict = {"Blocks used": blocks_used, "Completed challenges": completed_challenges, "Shares": shares}
+        number_of_items = 3
+
         index = 0
 
         for title, stat in stat_dict.iteritems():
 
             box = Gtk.EventBox()
             box.get_style_context().add_class("white")
-            box.set_size_request(WINDOW_WIDTH / 4, self.height)
+            box.set_size_request(WINDOW_WIDTH / number_of_items, self.height)
             box_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
 
             stat_label = Gtk.Label(stat)
             stat_label.get_style_context().add_class("stat_number")
             stat_label.set_alignment(xalign=0.5, yalign=1)
-            stat_label.set_size_request(WINDOW_WIDTH / 4, self.height / 2)
+            stat_label.set_size_request(WINDOW_WIDTH / number_of_items, self.height / 2)
 
             stat_title = Gtk.Label(title)
             stat_title.get_style_context().add_class("stat_title")
             stat_title.set_alignment(xalign=0.5, yalign=0)
-            stat_title.set_size_request(WINDOW_WIDTH / 4, self.height / 2)
+            stat_title.set_size_request(WINDOW_WIDTH / number_of_items, self.height / 2)
 
             box_container.pack_start(stat_label, False, False, 0)
             box_container.pack_start(stat_title, False, False, 0)

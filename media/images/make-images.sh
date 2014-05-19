7012820303d2af0122a7f21615c9f2108b0846cf
Zsolt Ero <zsolt.ero@gmail.com>
2014-05-19 14:56:17 +0100

    make-images added

---
+++ media/images/make-images.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+./generate-locked-versions.py
+
+level=2
+
+cd avatars
+./generate-images.sh $level
+cd ..
+
+cd badges
+./generate-images.sh $level
+cd ..
+
+cd environments
+./generate-images.sh $level
+cd ..
+
+cd progress_icons
+./generate-images.sh $level
+cd ..
+
+gfind -path '*.tmp*' -exec rm {} \;

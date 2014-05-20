bf7c7633f0f6e076e375f1b152714565b8245bd7
Zsolt Ero <zsolt.ero@gmail.com>
2014-05-20 15:57:06 +0100

    levelup images

--- media/images/badges/generate-images.sh
+++ media/images/badges/generate-images.sh
@@ -2,6 +2,8 @@
 
 kiwimasher -t png8 -s png -o -c$1 -p resize_fill 230 180 --no-suffix
 kiwimasher -t png8 -s png -o -c$1 -p resize_fill 460 448 --no-suffix
+kiwimasher -t png8 -s png -o -c$1 -p resize_fill 590 270 --no-suffix
+
 
 
 

2428170702b200721c80fe40a1ed389244acd547
Zsolt Ero <zsolt.ero@gmail.com>
2014-09-12 14:31:30 +0200

    disabling levelup checking for badges

--- tools/check_images.py
+++ tools/check_images.py
@@ -31,7 +31,7 @@ for category, subcats in all_rules.iteritems():
         needed_items_locked = ['{}_locked.png'.format(f) for f in items.keys()]
         needed_items_circular = ['{}_circular.png'.format(f) for f in items.keys()]
         needed_items_white_circular = ['{}_white_circular.png'.format(f) for f in items.keys()]
-        needed_items_levelup = ['{}_levelup.png'.format(f) for f in items.keys()]
+        # needed_items_levelup = ['{}_levelup.png'.format(f) for f in items.keys()]
 
         if category == 'avatars':
             # needed_items_levelup = uniqify_list(['{}_levelup.png'.format(f[:-2]) for f in items.keys()])
@@ -39,7 +39,7 @@ for category, subcats in all_rules.iteritems():
                 needed_items_white_circular
 
         if category == 'badges':
-            needed_items = needed_items_images + needed_items_locked + needed_items_levelup
+            needed_items = needed_items_images + needed_items_locked
 
         if category == 'environments':
             needed_items = needed_items_images + needed_items_locked

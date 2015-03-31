50555b734327d16a1df27d55308f96628a4fab44
Costas Voloudakis <costas@kano.me>
2015-03-31 16:24:22 +0100

    Add avatar_generator configuration file

--- kano_avatar/paths.py
+++ kano_avatar/paths.py
@@ -6,7 +6,7 @@
 #
 import os
 
-AVATAR_CONF_FILE = 'sample_conf.yaml'
+AVATAR_CONF_FILE = '/usr/share/kano-profile/rules/avatar_generator/conf.yaml'
 AVATAR_ASSET_FOLDER = '/usr/share/kano-profile/media/images/avatar_generator'
 CHARACTER_DIR = os.path.join(AVATAR_ASSET_FOLDER, 'characters')
 ITEM_DIR = os.path.join(AVATAR_ASSET_FOLDER, 'items')

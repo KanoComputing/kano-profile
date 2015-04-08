3050e4bc434fe46786b5de6352882861c0d72745
carolineclark <caroline@kano.me>
2015-04-08 09:24:30 +0100

    Renamed CSS file

--- kano_avatar/paths.py
+++ kano_avatar/paths.py
@@ -8,9 +8,15 @@ import os
 
 AVATAR_CONF_FILE = '/usr/share/kano-profile/rules/avatar_generator/conf.yaml'
 AVATAR_ASSET_FOLDER = '/usr/share/kano-profile/media/images/avatar_generator'
+CSS_PATH = '/usr/share/kano-profile/media/CSS/avatar_generator.css'
+
 CHARACTER_DIR = os.path.join(AVATAR_ASSET_FOLDER, 'characters')
 ITEM_DIR = os.path.join(AVATAR_ASSET_FOLDER, 'items')
-CATEGORY_ICONS = os.path.join(AVATAR_ASSET_FOLDER, 'categories')
 
+CATEGORY_ICONS = os.path.join(AVATAR_ASSET_FOLDER, 'categories')
+ACTIVE_CATEGORY_ICONS = os.path.join(CATEGORY_ICONS, 'active')
+INACTIVE_CATEGORY_ICONS = os.path.join(CATEGORY_ICONS, 'inactive')
+
+PREVIEW_ICONS = os.path.join(AVATAR_ASSET_FOLDER, 'preview')
 CIRC_ASSET_MASK = os.path.join(AVATAR_ASSET_FOLDER, 'helper_assets', 'circle_mask.png')
 RING_ASSET = os.path.join(AVATAR_ASSET_FOLDER, 'helper_assets', 'grey_ring.png')

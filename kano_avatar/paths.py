e147ec7b730149b549fae56cd1cb3f73d46c5ed0
Costas Voloudakis <costas@kano.me>
2015-05-05 20:03:13 +0100

    Fix style in paths.py

--- kano_avatar/paths.py
+++ kano_avatar/paths.py
@@ -8,10 +8,8 @@
 import os
 from kano.utils import get_home, get_user
 
-AVATAR_SCRATCH = os.path.join('/tmp/',
-                              get_user(),
-                              'char_gen_scratch',
-                              'character.png')
+AVATAR_SCRATCH = os.path.join(
+    '/tmp/', get_user(), 'char_gen_scratch', 'character.png')
 AVATAR_DEFAULT_LOC = os.path.join(get_home(), '.character-content')
 AVATAR_DEFAULT_NAME = 'character.png'
 AVATAR_ENV_DEFAULT = 'character_inc_env.png'
@@ -21,25 +19,28 @@ AVATAR_SELECTED_ITEMS = os.path.join(AVATAR_DEFAULT_LOC, 'character_log.json')
 
 AVATAR_CONF_FILE = '/usr/share/kano-profile/rules/avatar_generator/conf.yaml'
 PROFILE_IMAGES_FOLDER = '/usr/share/kano-profile/media/images'
-AVATAR_ASSET_FOLDER = os.path.join(PROFILE_IMAGES_FOLDER,'avatar_generator')
+AVATAR_ASSET_FOLDER = os.path.join(PROFILE_IMAGES_FOLDER, 'avatar_generator')
 CSS_PATH = '/usr/share/kano-profile/media/CSS/avatar_generator.css'
 
 CHARACTER_DIR = os.path.join(AVATAR_ASSET_FOLDER, 'characters')
-ENVIRONMENT_DIR = os.path.join(PROFILE_IMAGES_FOLDER,
-                               'environments',
-                               '734x404',
-                               'all')
+ENVIRONMENT_DIR = os.path.join(
+    PROFILE_IMAGES_FOLDER, 'environments', '734x404', 'all')
 ITEM_DIR = os.path.join(AVATAR_ASSET_FOLDER, 'items')
 
 CATEGORY_ICONS = os.path.join(AVATAR_ASSET_FOLDER, 'category_icons')
 ACTIVE_CATEGORY_ICONS = os.path.join(CATEGORY_ICONS, 'active')
 INACTIVE_CATEGORY_ICONS = os.path.join(CATEGORY_ICONS, 'inactive')
 
-SPECIAL_CATEGORY_ICONS = os.path.join(AVATAR_ASSET_FOLDER, 'special_category_icons')
+SPECIAL_CATEGORY_ICONS = os.path.join(
+    AVATAR_ASSET_FOLDER, 'special_category_icons')
 ACTIVE_SPECIAL_CATEGORY_ICONS = os.path.join(SPECIAL_CATEGORY_ICONS, 'active')
-INACTIVE_SPECIAL_CATEGORY_ICONS = os.path.join(SPECIAL_CATEGORY_ICONS, 'inactive')
+INACTIVE_SPECIAL_CATEGORY_ICONS = os.path.join(
+    SPECIAL_CATEGORY_ICONS, 'inactive')
 
 PREVIEW_ICONS = os.path.join(AVATAR_ASSET_FOLDER, 'preview')
-CIRC_ASSET_MASK = os.path.join(AVATAR_ASSET_FOLDER, 'helper_assets', 'circle_mask.png')
-RING_ASSET = os.path.join(AVATAR_ASSET_FOLDER, 'helper_assets', 'grey_ring.png')
-PLAIN_MASK = os.path.join(AVATAR_ASSET_FOLDER, 'helper_assets', 'plain_mask.png')
+CIRC_ASSET_MASK = os.path.join(
+    AVATAR_ASSET_FOLDER, 'helper_assets', 'circle_mask.png')
+RING_ASSET = os.path.join(
+    AVATAR_ASSET_FOLDER, 'helper_assets', 'grey_ring.png')
+PLAIN_MASK = os.path.join(
+    AVATAR_ASSET_FOLDER, 'helper_assets', 'plain_mask.png')

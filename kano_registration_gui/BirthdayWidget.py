f08731ac05a1f5e2762d50a9d23c83f543c6cc5f
Tom Bettany <tom@kano.me>
2015-06-22 11:15:45 +0100

    Register: Validate birthday as it is being input
    KanoComputing/peldins#1876
    Perform validation on the birthday as it is being typed in a similar
    fashion to the username and password entries.

--- kano_registration_gui/BirthdayWidget.py
+++ kano_registration_gui/BirthdayWidget.py
@@ -16,19 +16,27 @@ from kano.utils import is_number
 class BirthdayWidget(Gtk.Box):
 
     __gsignals__ = {
-        'bday-key-release-event': (GObject.SIGNAL_RUN_FIRST, None, ())
+        'bday-key-release-event': (GObject.SIGNAL_RUN_FIRST, None, ()),
+        'birthday-valid': (GObject.SIGNAL_RUN_FIRST, None, ()),
+        'birthday-invalid': (GObject.SIGNAL_RUN_FIRST, None, ()),
     }
+
+    FIELD_INCOMPLETE = 0
+    NOT_A_NUMBER = -1
 
     def __init__(self, day=None, month=None, year=None):
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL)
         self._create_birthday_widget(day, month, year)
 
     def _create_birthday_widget(self, day=None, month=None, year=None):
-
         # TODO: Establish how DD, MM, YYYY should be translated
         self._day_entry = self._create_entry("DD", 60, 2, day)
         self._month_entry = self._create_entry("MM", 60, 2, month)
         self._year_entry = self._create_entry("YYYY", 70, 4, year)
+
+        self._day_entry.connect('key-release-event', self.validate)
+        self._month_entry.connect('key-release-event', self.validate)
+        self._year_entry.connect('key-release-event', self.validate)
 
         label1 = self._create_separater_label()
         label2 = self._create_separater_label()
@@ -40,16 +48,26 @@ class BirthdayWidget(Gtk.Box):
         hbox.pack_start(label2, False, False, 0)
         hbox.pack_start(self._day_entry, False, False, 0)
 
-        birthday_label = Gtk.Label(_("Birthday"), xalign=0)
+        label_box = Gtk.Box()
+
+        birthday_label = Gtk.Label(_("Birthday"))
         birthday_label.get_style_context().add_class("get_data_label")
-        self.pack_start(birthday_label, False, False, 0)
+        label_box.pack_start(birthday_label, False, False, 0)
+
+        self._birthday_status = Gtk.Label()
+        self._birthday_status.get_style_context().add_class("validation_label")
+        label_box.pack_start(self._birthday_status, False, False, 0)
+
+        self.pack_start(label_box, False, False, 0)
         self.pack_start(hbox, False, False, 0)
 
         self.set_margin_left(30)
 
     def _create_entry(self, placeholder_text, width=60, char_num=2, entry_text=None):
         entry = Gtk.Entry()
+
         entry.set_size_request(width, -1)
+
         entry.set_width_chars(char_num)
         entry.set_max_length(char_num)
         entry.set_placeholder_text(placeholder_text)
@@ -60,6 +78,60 @@ class BirthdayWidget(Gtk.Box):
             entry.set_text(str(entry_text))
 
         return entry
+
+    def validate(self, *dummy):
+        day = self._str_to_int(self._day_entry.get_text())
+        month = self._str_to_int(self._month_entry.get_text())
+        year = self._str_to_int(self._year_entry.get_text())
+
+        if self.FIELD_INCOMPLETE in [day, month, year]:
+            return self._set_validation_msg('', False)
+
+        if self.NOT_A_NUMBER in [day, month, year]:
+            return self._set_validation_msg(_('must be a number'), False)
+
+        try:
+            birthday = datetime.date(year, month, day)
+        except Exception as e:
+            return self._set_validation_msg(_(e.message), False)
+
+        today = datetime.date.today()
+        if birthday > today:
+            return self._set_validation_msg(_('date is in the future'), False)
+
+        oldest_date = today.replace(year=(today.year - 100))
+        if birthday < oldest_date and (birthday.year < 0 or birthday.year > 99):
+            return self._set_validation_msg(_('year out of range'), False)
+
+        return self._set_validation_msg(_('is valid'), True)
+
+
+    def _str_to_int(self, string):
+        if len(string) == 0:
+            return self.FIELD_INCOMPLETE
+
+        if not string.isdigit():
+            return self.NOT_A_NUMBER
+
+        return int(string)
+
+
+    def _set_validation_msg(self, message, valid):
+        label = self._birthday_status
+        style = label.get_style_context()
+
+        style.remove_class('fail')
+        style.remove_class('success')
+
+        if valid:
+            style.add_class('success')
+            self.emit('birthday-valid')
+        else:
+            style.add_class('fail')
+            self.emit('birthday-invalid')
+
+        self._birthday_status.set_text(message)
+
 
     def _emit_key_press(self, widget, event):
         self.emit("bday-key-release-event")

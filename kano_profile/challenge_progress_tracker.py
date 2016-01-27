32264f7d0f4e2fb5478b6ee7e1acbb25050ec5ac
Costas Voloudakis <costas@kano.me>
2016-01-27 16:32:56 +0000

    Add app name to challenge progress tracker
    Also make it absolutely clear that this class is a singleton

--- kano_profile/challenge_progress_tracker.py
+++ kano_profile/challenge_progress_tracker.py
@@ -19,12 +19,44 @@ class ChallengeProgressTracker(object):
     COMPLETED = 'completed'
     ABANDONED = 'abandoned'
 
+    _singleton_instance = None
+
+    @staticmethod
+    def get_instance():
+        if not ChallengeProgressTracker._singleton_instance:
+            ChallengeProgressTracker()
+
+        return ChallengeProgressTracker._singleton_instance
+
     def __init__(self):
+        if ChallengeProgressTracker._singleton_instance:
+            raise Exception('This class is a singleton!')
+        else:
+            ChallengeProgressTracker._singleton_instance = self
+
+        self._app_name = ''
         self._tracking_mode = None
         self._chal_group = None
         self._chal_no = None
         self._chal_outcome = None
         self._app_session_id = str(uuid4())
+
+    def set_name(self, app_name):
+        if not isinstance(app_name, basestring):
+            raise TypeError('set_name argument must be of type string')
+
+        if app_name:
+            self._app_name = app_name
+        else:
+            raise ValueError('App name must not be empty')
+
+    def _check_name_is_set(self):
+        if not self._app_name:
+            raise RuntimeError(
+                'App name is not set for this instance of {}. '
+                'Please run the set_name method'
+                .format(self.__class__.__name__)
+            )
 
     def clear_current_tracking(self):
         if self._tracking_mode == ChallengeProgressTracker.CHALLENGE:
@@ -40,6 +72,7 @@ class ChallengeProgressTracker(object):
         self._chal_outcome = None
 
     def start_challenge(self, challenge_group, challenge_no):
+        self._check_name_is_set()
         self.clear_current_tracking()
 
         self._tracking_mode = ChallengeProgressTracker.CHALLENGE
@@ -49,12 +82,14 @@ class ChallengeProgressTracker(object):
         self._start_timestamp = datetime.now()
 
     def unlocked_challege(self, challenge_group, challenge_no):
+        self._check_name_is_set()
         if self._tracking_mode == ChallengeProgressTracker.CHALLENGE and \
                 self._chal_group == challenge_group and \
                 self._chal_no == challenge_no:
             self._chal_outcome = ChallengeProgressTracker.UNLOCKED
 
     def completed_challenge(self, challenge_group, challenge_no):
+        self._check_name_is_set()
         if self._tracking_mode == ChallengeProgressTracker.CHALLENGE and \
                 self._chal_group == challenge_group and \
                 self._chal_no == challenge_no and \
@@ -71,6 +106,7 @@ class ChallengeProgressTracker(object):
                 tracker.track_data(
                     'challenge-attempt',
                     {
+                        'app_name': self._app_name,
                         'app_session': self._app_session_id,
                         'challenge_group': self._chal_group,
                         'challenge_no': self._chal_no,
@@ -88,6 +124,7 @@ class ChallengeProgressTracker(object):
         self._start_timestamp = None
 
     def start_playground(self, hw_ref):
+        self._check_name_is_set()
         self.clear_current_tracking()
 
         self._tracking_mode = ChallengeProgressTracker.PLAYGROUND
@@ -102,6 +139,7 @@ class ChallengeProgressTracker(object):
                 tracker.track_data(
                     'playground-session',
                     {
+                        'app_name': self._app_name,
                         'app_session': self._app_session_id,
                         'hw_ref': self._current_hw_ref,
                         'length_sec': attempt_length.total_seconds()
@@ -116,6 +154,3 @@ class ChallengeProgressTracker(object):
             self.stop_challenge(self._chal_group, self._chal_no)
         elif self._tracking_mode == ChallengeProgressTracker.PLAYGROUND:
             self.stop_playground(self._current_hw_ref)
-
-
-challenge_progress_tracker = ChallengeProgressTracker()

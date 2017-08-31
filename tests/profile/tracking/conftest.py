d0493e2d7ba61a312e5fc50b8e72b0df88afcc81
Tom Bettany <tom@kano.me>
2017-08-31 11:11:22 +0100

    Tests: Separate tracking utilities into fixtures
    Split out the tracking functions and sample tracking sessions into
    fixtures to be reusable across many tests.

---
+++ tests/profile/tracking/conftest.py
@@ -0,0 +1,108 @@
+#
+# conftest.py
+#
+# Copyright (C) 2017 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
+#
+# Definition of fixtures for tracking tests
+#
+
+
+import os
+import json
+from uuid import uuid1, uuid5
+import pytest
+
+from kano.utils.file_operations import ensure_dir
+from kano_profile.tracker.tracking_session import TrackingSession
+from kano_profile.paths import tracker_dir, PAUSED_SESSIONS_FILE
+
+
+class TrackingSessionFixture(object):
+    @staticmethod
+    def format_session(name, started, pid, elapsed, finished):
+        return {
+            "name": name,
+            "started": started,
+            "pid": pid,
+            "elapsed": elapsed,
+            "finished": finished,
+            "app_session_id": str(uuid5(uuid1(), '{}{}'.format(name, pid)))
+        }
+
+    @classmethod
+    def setup_session_fixtures(cls, directory, sessions):
+        ensure_dir(directory)
+        for session_f in os.listdir(directory):
+            session_path = os.path.join(directory, session_f)
+            if os.path.isfile(session_path):
+                os.remove(session_path)
+
+        for session in sessions:
+            session_obj = TrackingSession(
+                name=session['name'],
+                pid=session['pid']
+            )
+            session_path = os.path.join(directory, session_obj.file)
+
+            with open(session_path, 'w') as session_f:
+                json.dump(session, session_f)
+
+
+    @classmethod
+    def setup_sessions(cls, sessions):
+        cls.setup_session_fixtures(tracker_dir, sessions)
+
+
+    @classmethod
+    def setup_paused_sessions(cls, sessions):
+        if sessions == None:
+            if os.path.exists(PAUSED_SESSIONS_FILE):
+                os.remove(PAUSED_SESSIONS_FILE)
+
+            return
+
+        with open(PAUSED_SESSIONS_FILE, 'w') as sessions_f:
+            for session in sessions:
+                sessions_f.write(
+                    '{}\n'.format(json.dumps({
+                        'name': session['name'],
+                        'pid': session['pid']
+                    }))
+                )
+
+
+@pytest.fixture(scope='function')
+def sample_tracking_sessions():
+    return [
+        TrackingSessionFixture.format_session(
+            'test-1', 12345678, 1234, 60, True
+        ),
+        TrackingSessionFixture.format_session(
+            'test-2', 22345678, 1234, 32, True
+        ),
+        TrackingSessionFixture.format_session(
+            'test-3', 32345678, 1234, 288, True
+        ),
+        TrackingSessionFixture.format_session(
+            'test-4', 42345678, 1234, 119, False
+        ),
+        TrackingSessionFixture.format_session(
+            'test-5', 52345678, 1234, 3, False
+        ),
+    ]
+
+
+@pytest.fixture(scope='module')
+def tracking_session():
+    return TrackingSessionFixture()
+
+
+@pytest.fixture(scope='module')
+def tracking_session_file_data():
+    return {
+        'file': '1242-test-file-1.json',
+        'path': os.path.join(tracker_dir, '1242-test-file-1.json'),
+        'pid': 1242,
+        'name': 'test-file-1'
+    }
--- tests/profile/tracking/test_tracking_session.py
+++ tests/profile/tracking/test_tracking_session.py
@@ -9,29 +9,14 @@
 #
 
 
-import os
-import pytest
-
-from kano_profile.paths import tracker_dir
 from kano_profile.tracker.tracking_session import TrackingSession
 
 
-TEST_DATA = [
-    {
-        'file': '1242-test-file-1.json',
-        'path': os.path.join(tracker_dir, '1242-test-file-1.json'),
-        'pid': 1242,
-        'name': 'test-file-1'
-    },
-]
-
-
-@pytest.mark.parametrize('test_data', TEST_DATA)
-def test_file_init(test_data):
-    session_file = test_data['file']
-    session_path = test_data['path']
-    session_pid = test_data['pid']
-    session_name = test_data['name']
+def test_file_init(tracking_session_file_data):
+    session_file = tracking_session_file_data['file']
+    session_path = tracking_session_file_data['path']
+    session_pid = tracking_session_file_data['pid']
+    session_name = tracking_session_file_data['name']
 
     session = TrackingSession(session_file=session_file)
 
@@ -41,12 +26,11 @@ def test_file_init(test_data):
     assert session.name == session_name
 
 
-@pytest.mark.parametrize('test_data', TEST_DATA)
-def test_pid_name_init(test_data):
-    session_file = test_data['file']
-    session_path = test_data['path']
-    session_pid = test_data['pid']
-    session_name = test_data['name']
+def test_pid_name_init(tracking_session_file_data):
+    session_file = tracking_session_file_data['file']
+    session_path = tracking_session_file_data['path']
+    session_pid = tracking_session_file_data['pid']
+    session_name = tracking_session_file_data['name']
 
     session = TrackingSession(pid=session_pid, name=session_name)
 

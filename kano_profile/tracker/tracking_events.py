508ef63e8326ec7d099877ccafb2b5b0e28b0c2c
Radu <radu@kano.me>
2017-09-26 12:05:03 +0100

    Added kit type and version to the hw-info event

--- kano_profile/tracker/tracking_events.py
+++ kano_profile/tracker/tracking_events.py
@@ -2,7 +2,7 @@
 
 # tracking-events.py
 #
-# Copyright (C) 2015 Kano Computing Ltd.
+# Copyright (C) 2015-2017 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 #
 # A few predefined tracking events
@@ -15,10 +15,33 @@ from kano.utils import get_rpi_model, detect_kano_keyboard, get_partition_info
 
 
 def _hw_info():
+    # TODO: This import is done here to avoid potential
+    #       import loops with kano-peripherals.
+    from kano_peripherals.wrappers.detection import is_ck2_lite, is_ck2_pro, \
+        get_ck2_lite_version, get_ck2_pro_version
+
+    # If CKL/CKCs cannot be detected, defaults to Computer Kits without version.
+    kit_type = 'ck'
+    kit_version = None
+
+    # Detect CK Lite via the PiHat board.
+    if is_ck2_lite():
+        kit_type = 'ckl'
+        kit_version = get_ck2_lite_version()
+
+    # Detect CK Complete via the PowerHat board and other peripherals.
+    elif is_ck2_pro():
+        kit_type = 'ckc'
+        kit_version = get_ck2_pro_version()
+
     track_data('hw-info', {
         'keyboard': 'kano' if detect_kano_keyboard() else 'generic',
         'model': get_rpi_model(),
-        'partitions': get_partition_info()
+        'partitions': get_partition_info(),
+        'kit': {
+            'type': kit_type,
+            'version': kit_version.vstring if kit_version else kit_version
+        }
     })
 
 
@@ -34,6 +57,7 @@ def _ping():
     if not status:
         msg = _("Failed to send the ping event ({})").format(err)
         raise RuntimeError(msg)
+
 
 def _low_battery():
     track_data('battery', {

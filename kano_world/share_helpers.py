cc858a2c851553e15c145e3145642b91b3959667
Radu <radu@kano.me>
2017-10-06 10:57:05 +0100

    Launch kano-login localised when attempting to share
    The login app is already localised, but it is being launched without
    translations from apps that use login_and_share(), namely Make Art
    and Make Light.

--- kano_world/share_helpers.py
+++ kano_world/share_helpers.py
@@ -1,9 +1,11 @@
 # share_helpers.py
 #
-# Copyright (C) 2016 Kano Computing Ltd.
+# Copyright (C) 2016-2017 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
-#
+
+
 from kano.utils.shell import run_cmd
+
 from .functions import login_using_token
 from .share import upload_share
 
@@ -27,7 +29,7 @@ def login_and_share(file_path, title, app_name):
     success, unused = login_using_token()
 
     if not success:
-        run_cmd('kano-login -r')
+        run_cmd('kano-login -r', localised=True)
         success, unused = login_using_token()
         if not success:
             return False, _("Cannot login")
